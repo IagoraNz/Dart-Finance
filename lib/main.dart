3b39cffe95c9b43c86fbe830caceb6e5476ad1c2
IagoraNz <iago.almeida@ufpi.edu.br>
2024-03-03 19:13:26 -0300

    Backup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
 
-main() => runApp(const ExpensesApp());
+void main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({super.key});
+  const ExpensesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,32 +15,56 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     const Color appBarColor = Color.fromARGB(255, 117, 163, 201);
+    final DateTime currentDate = DateTime.now();
+
+    final List<Transaction> _transactions = [
+      Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: currentDate,
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 211.30,
+        date: currentDate,
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
         centerTitle: true,
         backgroundColor: appBarColor,
       ),
-      body: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
+          const SizedBox(
             width: double.infinity,
             child: Card(
               color: Color.fromARGB(255, 117, 163, 201),
               child: Text('Gráfico'),
             ),
           ),
-          Card(
-            child: Text('Lista de transações'),
+          Column(
+            children: _transactions.map((tr) {
+              /*.map(tr) vai percorrer cada elemento da lista e retornar um novo elemento
+              na lista, que será um Card com o título da transação*/
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
           )
         ],
-      )
+      ),
     );
   }
 }
